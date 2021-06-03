6976110c5e80bcd16325a9e2832f52399c677b1f
aldop <aldop@MSI>
2021-06-03 01:53:05 -0500

    Solo agregado de imagen necesito resolver algunas dudas al dia siguiente

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,5 +20,8 @@ public class Main {
     }
     public static void main(String[] args) {
         new Main();
+       // Principal P = new Principal();
+       // P.setVisible(true);
+        
     }
 }
