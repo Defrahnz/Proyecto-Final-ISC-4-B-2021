274011f9caf4c7e19eabc7660dd0fdd727556e5b
aldop <aldop@MSI>
2021-06-08 19:44:43 -0500

    Panel de registro de huespedes en colaboracion con todo el team

--- src/clases/RegistrarHuesped.java
+++ src/clases/RegistrarHuesped.java
@@ -27,11 +27,69 @@ public class RegistrarHuesped extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jTextFieldNombreH = new javax.swing.JTextField();
+        jTextFieldTelefonoH = new javax.swing.JTextField();
+        jTextFieldDomicilioH = new javax.swing.JTextField();
+        jTextFieldNacimientoH = new javax.swing.JTextField();
+        jTextFieldCurpH = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("REGISTRAR HUESPED");
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Baskerville Old Face", 1, 18)); // NOI18N
+        jLabel2.setText("Nombre");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 150, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Baskerville Old Face", 1, 18)); // NOI18N
+        jLabel3.setText("Numero de telefono");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 190, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("Baskerville Old Face", 1, 18)); // NOI18N
+        jLabel4.setText("Domicilio");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 230, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Baskerville Old Face", 1, 18)); // NOI18N
+        jLabel5.setText("Fecha de nacimiento");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 270, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Baskerville Old Face", 1, 18)); // NOI18N
+        jLabel6.setText("Curp");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 310, -1, -1));
+
+        jTextFieldNombreH.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldNombreHActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextFieldNombreH, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 140, 260, 30));
+
+        jTextFieldTelefonoH.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldTelefonoHActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextFieldTelefonoH, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 180, 260, 30));
+
+        jTextFieldDomicilioH.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldDomicilioHActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextFieldDomicilioH, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 220, 260, 30));
+        getContentPane().add(jTextFieldNacimientoH, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 260, 260, 30));
+        getContentPane().add(jTextFieldCurpH, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 300, 260, 30));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Logomini.png"))); // NOI18N
+        jLabel7.setText("jLabel7");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 30, 70, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo1.jpg"))); // NOI18N
         jLabel1.setText("jLabel1");
@@ -39,6 +97,18 @@ public class RegistrarHuesped extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextFieldTelefonoHActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldTelefonoHActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldTelefonoHActionPerformed
+
+    private void jTextFieldDomicilioHActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldDomicilioHActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldDomicilioHActionPerformed
+
+    private void jTextFieldNombreHActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldNombreHActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldNombreHActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,5 +147,16 @@ public class RegistrarHuesped extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JTextField jTextFieldCurpH;
+    private javax.swing.JTextField jTextFieldDomicilioH;
+    private javax.swing.JTextField jTextFieldNacimientoH;
+    private javax.swing.JTextField jTextFieldNombreH;
+    private javax.swing.JTextField jTextFieldTelefonoH;
     // End of variables declaration//GEN-END:variables
 }
