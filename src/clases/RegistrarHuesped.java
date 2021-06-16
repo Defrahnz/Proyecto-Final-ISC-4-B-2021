24f9bfb6995bbeac45b4c9a5783a406b48ac82b9
lapiz <lapiz@DESKTOP-KV70QRJ>
2021-06-16 18:58:54 -0500

    comentario de prueba

--- src/clases/RegistrarHuesped.java
+++ src/clases/RegistrarHuesped.java
@@ -29,7 +29,7 @@ public class RegistrarHuesped extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(null);
     }
-
+   //Hola mundo//
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
